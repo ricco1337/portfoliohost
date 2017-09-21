02f8cdaa0a7ae76a468cf7aafea257fe0ab5b0d6
yourGitHubusername <ricco1337@outlook.com>
2017-09-21 14:09:20 -0600

    changing host

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.use(express.static(path.join(__dirname, 'build')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build/index.html'))
 });
-let port = 3001
+let port = 80
 app.listen(port,() => console.log(`lisening on port ${port}`))
